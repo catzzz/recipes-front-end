fffb43ca8fc4f988e426bc70992ec3d9ba95ece5
Catzzz <chuneleu@gmail.com>
2021-07-14 01:51:40 -0700

    add hover and transition effect

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,6 @@
 const Card = ({ image, name, creator, duration }) => {
   return (
-    <div className="card">
+    <div className="card hover:shadow-lg">
       <img
         src={image}
         alt={name}
@@ -11,6 +11,20 @@ const Card = ({ image, name, creator, duration }) => {
         <span className="block text-gray-500">Recipe by {creator}</span>
       </div>
       <div className="badge">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-5 inline-block mr-1"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
         <span>{duration} mins</span>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -4,26 +4,32 @@ import Link from "next/link";
 import CardList from "../components/CardList";
 import Navbar from "../components/Navbar";
 import { API_URL } from "../config/index";
+import {useState} from 'react'
 export default function Home({ recipes }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const toggleMenu = () =>{
+    
+    setIsMenuOpen(!isMenuOpen)
+  }
   return (
     // content wrapper
-    <div className="text-gray-600 font-body ">
+    <div className="text-gray-500 font-body ">
       <div className="grid md:grid-cols-3">
-        <Navbar />
+        <Navbar isMenuOpen={isMenuOpen} toggleMenu={toggleMenu}/>
         {/* end nav */}
         <main className="px-16 py-6 bg-gray-100 md:col-span-2">
           <div className="flex justify-center md:justify-end">
             <Link href="#">
-              <a className="btn text-primary border-primary md:border-2">Log in</a>
+              <a className="btn text-primary border-primary md:border-2 hover:bg-primary hover:text-gray-100 transition ease-out duration-500">Log in</a>
             </Link>
             <Link href="#">
-              <a className="btn text-primary ml-2 border-primary md:border-2">Sign up</a>
+              <a className="btn text-primary ml-2 border-primary md:border-2 hover:text-gray-100 hover:bg-primary transition ease-out duration-500">Sign up</a>
             </Link>
           </div>
 
           <header>
-            <h2 className="text-gray-700 text-6xl font-semibold">Recipes</h2>
-            <h3 className="text=2xl font-semibold">For Bee Chu</h3>
+            <h2 className="text-gray-700 text-6xl font-semibold ">Recipes</h2>
+            <h3 className="text=2xl font-semibold  ">For Bee Chu</h3>
           </header>
 
           <div>
@@ -36,7 +42,7 @@ export default function Home({ recipes }) {
             <CardList title={"Most Popular"} recipes={recipes} />
 
             <div className="flex justify-center mt-8">
-              <div className="btn bg-secondary-100 text-secondary-200">
+              <div className="btn bg-secondary-100 text-secondary-200 hover:shadow-inner transform hover:scale-105 hover:bg-opacity-60 transition ease-out duration-300">
                 Load more
               </div>
             </div>
